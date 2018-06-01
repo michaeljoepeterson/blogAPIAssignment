49036b1b95d3e2ec4b2ce8cad388b4216c51dfb1
Michael Peterson <michaeljoepeterson@gmail.com>
2018-05-31 21:26:15 -0600

    finished all requests

--- blogRouter.js
+++ blogRouter.js
@@ -14,4 +14,56 @@ router.get('/', (req, res) => {
   res.json(BlogPosts.get());
 });
 
+router.post('/', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['title', 'content', "author"];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author,req.body.publishDate);
+  res.status(201).json(item);
+});
+
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted item \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', "author"];
+
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  });
+
+  res.status(204).end();
+});
+
 module.exports = router;
--- models.js
+++ models.js
@@ -47,15 +47,19 @@ const BlogPosts = {
     }
   },
   update: function(updatedPost) {
+    
     const {id} = updatedPost;
     const postIndex = this.posts.findIndex(
       post => post.id === updatedPost.id);
+    
     if (postIndex === -1) {
       throw new StorageException(
         `Can't update item \`${id}\` because doesn't exist.`)
     }
+    
     this.posts[postIndex] = Object.assign(
       this.posts[postIndex], updatedPost);
+    
     return this.posts[postIndex];
   }
 };
